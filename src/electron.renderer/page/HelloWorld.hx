b8abfdaf23c38367ea08a41a40e1d592bf146270
Sébastien Bénard <github@deepnight.net>
2021-03-22 13:31:08 +0100

    Cleanup hello world

--- src/electron.renderer/page/HelloWorld.hx
+++ src/electron.renderer/page/HelloWorld.hx
@@ -4,6 +4,5 @@ class HelloWorld extends Page {
 	public function new() {
 		super();
 		loadPageTemplate("helloWorld.html");
-		jPage.find("button").click( _->App.ME.loadPage( ()->new HelloWorld() ) );
 	}
 }
